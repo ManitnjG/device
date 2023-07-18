fb9f87d50cf2e89c0d9c2e31020484e0b9c89d0b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-07-18 19:28:04 +0530

    Update and rename evolution_X01BD.mk to arrow_X01BD.mk

--- arrow_X01BD.mk
+++ arrow_X01BD.mk
@@ -10,9 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Evolution-X stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
 
-EVO_BUILD_TYPE := CUSTOM
+
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
@@ -20,7 +20,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_X01BD
+PRODUCT_NAME := arrow_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Asus Zenfone Max Pro M2
