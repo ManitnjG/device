984725ec23da2d0129d142582a31a7b32682ebbb
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-07-18 20:31:30 +0530

    Update arrow_X01BD.mk

--- arrow_X01BD.mk
+++ arrow_X01BD.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Evolution-X stuff
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 
 
