b0ef04f12c8e834ef7c7a9638c289af91ec7db89
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-07-29 12:24:53 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The Evolution-X Project
+# Copyright (C) 2020 The corvus-X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_X01BD.mk
+    $(LOCAL_DIR)/corvus_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_X01BD-user \
-    evolution_X01BD-userdebug
+    corvus_X01BD-user \
+    corvus_X01BD-userdebug
