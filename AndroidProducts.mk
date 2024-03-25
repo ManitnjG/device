e5682f906de363a4897cee9733e61f757a268c80
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-25 18:30:09 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The corvus-X Project
+# Copyright (C) 2020 The nad-X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_X01BD.mk
+    $(LOCAL_DIR)/nad_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_X01BD-user \
-    corvus_X01BD-userdebug
+    nad_X01BD-user \
+    nad_X01BD-userdebug
