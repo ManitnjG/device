a22367b29a57bb1c7f45ccd19a6b8a1eb42d9c34
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-11 06:51:11 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The nad-X Project
+# Copyright (C) 2020 The superior-X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_X01BD.mk
+    $(LOCAL_DIR)/superior_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_X01BD-user \
-    nad_X01BD-userdebug
+    superior_X01BD-user \
+    superior_X01BD-userdebug
